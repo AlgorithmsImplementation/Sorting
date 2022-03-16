3eda3fa09994744156de4ff35d7fbc796960e795
Geethika Kolly <geethika@Geethikas-MacBook-Air.local>
2022-03-15 20:39:51 -0400

    Updated the selection sort

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,11 +1,30 @@
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 
 /**
- *
+ * Selection sort basically do a linear scan and move the element to the first
  */
+@Slf4j
 public class SelectionSort implements Sort{
     @Override
     public ArrayList<Integer> sortElements(ArrayList<Integer> elements) {
-        return null;
+
+        for(int pass = 1; pass < elements.size() ; pass ++) {
+            //in the first pass we will sort the first element
+            int minPos = pass-1;
+            for(int i = pass-1 ; i < elements.size() ; i++){
+                if(elements.get(i) < elements.get(minPos)){
+                    minPos = i;
+                }
+            }
+
+            log.info("Pass {}", pass);
+
+            int temp = elements.get(pass-1);
+            elements.set(pass-1, elements.get(minPos));
+            elements.set(minPos, temp);
+        }
+        return elements;
     }
 }
