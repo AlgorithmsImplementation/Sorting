b36b8d3cc74c8a1dbbbcf77437a18a6023e8f3f6
Geethika Kolly <geethika@Geethikas-MacBook-Air.local>
2022-03-29 22:13:21 -0400

    Updated the doc

--- src/main/java/Sorting.java
+++ src/main/java/Sorting.java
@@ -1,4 +1,8 @@
 public class Sorting {
+    /**
+     * Main class implementation to call different sorting algorithms
+     * @param args
+     */
     public static void main(String[] args){
 
     }
